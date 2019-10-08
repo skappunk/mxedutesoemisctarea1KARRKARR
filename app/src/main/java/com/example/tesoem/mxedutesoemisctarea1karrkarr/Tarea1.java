e95ed045b361f3a973c58c510e7c2e5981fc5fae
ISC Tesoem <tesoem@iMac-Tesoem-01.local>
2019-10-07 19:16:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tesoem/mxedutesoemisctarea1karrkarr/Tarea1.java
@@ -0,0 +1,13 @@
+package com.example.tesoem.mxedutesoemisctarea1karrkarr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarea1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarea1);
+    }
+}
